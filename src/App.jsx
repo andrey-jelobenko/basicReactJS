12b713ea4b68aed6377c892d7c66b3f4f2e54e72
Andrey <ajelobenko@mail.ru>
2021-11-13 18:57:44 +0300

    ДЗ 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import { Message } from './Message';
 
 function App() {
+	const text = 'Компонент Message, отображающий переданный ему пропсом текст';
 	return (
 		<div className="App">
 			<header className="App-header">
+				<Message text={text} />
 				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
 			</header>
 		</div>
 	);
