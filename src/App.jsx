85babbae73d2e1779beb06f27a7c4c144fd09fb7
AndreyJelobenko <ajelobenko@mail.ru>
2021-11-17 13:26:51 +0300

    убрал лишнее после установки CRA

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-import { Message } from './Message';
+import "./App.css";
+import { Message } from "./Message";
 
 function App() {
-	const text = 'Компонент Message, отображающий переданный ему пропсом текст';
-	return (
-		<div className="App">
-			<header className="App-header">
-				<Message text={text} />
-				<img src={logo} className="App-logo" alt="logo" />
-			</header>
-		</div>
-	);
+  const text = "Компонент Message, отображающий переданный ему пропсом текст";
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Message text={text} />
+      </header>
+    </div>
+  );
 }
 
 export default App;
